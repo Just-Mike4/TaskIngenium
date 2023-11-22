35625e4e0531d1e317d4664807ad498294291d2a
Ezeana Michael <michael.c.ezeana@gmail.com>
2023-11-22 10:15:24 +0100

    small changes

--- TaskApp/errors/handlers.py
+++ TaskApp/errors/handlers.py
@@ -1,15 +1,18 @@
 from flask import Blueprint, render_template
 
+# Error blueprint
 errors=Blueprint('errors',__name__)
 
-#Handle error 404, not found route
+#Handle error 404, route not found 
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html'), 404
+
 #Handle error 403, No permission
 @errors.app_errorhandler(403)
 def error_403(error):
     return render_template('errors/403.html'), 403
+
 #Handle error , Internal server error
 @errors.app_errorhandler(500)
 def error_500(error):
